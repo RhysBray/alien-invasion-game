8dd156dc8419d73b930aace28f7bb1bf7e272195
Rhys Bray <rhysabray@hotmail.com>
2019-06-05 00:56:31 +0100

    hosted on firebase https://alien-invasion-game.firebaseapp.com

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-const numberOfDrones = 20;
+const numberOfDrones = 15;
 let invasionForce = [];
 
 const gameStart = () => {
@@ -8,10 +8,10 @@ const gameStart = () => {
 };
 
 const spawnInvasionForce = () => {
-  invasionForce.push({ type: "mothership", hp: 100 });
+  invasionForce.push({ type: "Mothership", hp: 60 });
 
   for (var drones = 0; drones <= numberOfDrones; drones++) {
-    invasionForce.push({ type: "drone", hp: 20 });
+    invasionForce.push({ type: "Drone", hp: 20 });
   }
 };
 
@@ -23,7 +23,7 @@ const newRound = () => {
 
 const showInvasionForce = () => {
   invasionForce.map(alien => {
-    alien.type === "mothership"
+    alien.type === "Mothership"
       ? (document.getElementById("leader").innerHTML +=
           "<p id='mother-ship'>" + alien.type + ": " + alien.hp + "</p>")
       : (document.getElementById("swarm").innerHTML +=
@@ -34,8 +34,57 @@ const showInvasionForce = () => {
 const shoot = () => {
   let randomIndex = Math.floor(Math.random() * invasionForce.length);
   invasionForce[randomIndex].hp -= 10;
+  checkMotherShip();
   deleteDead();
   newRound();
+};
+
+const scatterShot = () => {
+  let randomIndex1 = Math.floor(Math.random() * invasionForce.length);
+  let randomIndex2 = Math.floor(Math.random() * invasionForce.length);
+  let randomIndex3 = Math.floor(Math.random() * invasionForce.length);
+  let randomIndex4 = Math.floor(Math.random() * invasionForce.length);
+
+  invasionForce[randomIndex1].hp -= 10;
+  invasionForce[randomIndex2].hp -= 10;
+  invasionForce[randomIndex3].hp -= 5;
+  invasionForce[randomIndex4].hp -= 5;
+  checkMotherShip();
+  deleteDead();
+  newRound();
+};
+
+const bomb = () => {
+  let randomIndex = Math.floor(Math.random() * invasionForce.length);
+  invasionForce[randomIndex + 1].hp -= 10;
+  invasionForce[randomIndex + 2].hp -= 10;
+  invasionForce[randomIndex].hp -= 10;
+  invasionForce[randomIndex - 1].hp -= 10;
+  invasionForce[randomIndex - 2].hp -= 10;
+  checkMotherShip();
+  deleteDead();
+  newRound();
+};
+
+const nuke = () => {
+  invasionForce.map(alien => {
+    alien.hp -= 15;
+  });
+  checkMotherShip();
+  deleteDead();
+  newRound();
+};
+
+const checkMotherShip = () => {
+  if (invasionForce[0].hp <= 0) {
+    instaKill();
+  }
+};
+
+const instaKill = () => {
+  invasionForce.map(alien => {
+    alien.hp = 0;
+  });
 };
 
 const deleteDead = () => {
